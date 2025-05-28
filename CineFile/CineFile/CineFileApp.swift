b2ccb2a5f319d16560ae965c22b1337679b26606
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2025-05-28 08:59:02 -0300

    add xcode project

---
+++ CineFile/CineFile/CineFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFileApp.swift
+//  CineFile
+//
+//  Created by Livia Rosembach Oliveira on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
